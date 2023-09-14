4ad4cd4cd4cde984af2b44cfc768cde1489e7bc5
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-14 07:02:12 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+/* when we use " use strict" keyword . all of our JavaScript code will be treated as newer version by engine .
+In most of the JS , Reatc files we won't use semicolon.
+readability should be a factor .
+
+For Documentation use " MDN ". 
+original documentaion of javascript you can get at " ecma script [tc39]".
+*/
+
+let name = "shivam" // string datatype  =>  " "
+let age = 18 // number type ( range 2 to power 53)
+let islogedIn = true // boolean datatype  => True or False 
+let state ;
+
+/* some more datatypes
+Primite datatypes 
+bigInt 
+null => standalone value 
+undefined => Variable is decalred but value is not defined .
+symbol  = > unique 
+*/
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
