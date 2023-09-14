3c187d5b22f59f028cbda7c5351a653e91b4d700
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-14 06:20:55 +0000

    container config done

--- 01_Basics/01_Var.js
+++ 01_Basics/01_Var.js
@@ -3,11 +3,20 @@ const userId = 1001
 let email = "Shivi@abc.com"
 var password = "11224"
 userCity = "Shimla"
+let userState ;
 
 //userId = 1 // can't be changed  
 email = "shiv@xyz.com"
 password = "2345"
 userCity = "Mohali"
 console.log(userId);
-console.table([email,password,userCity]);
+console.table([email,password,userCity,userState]);
 
+/* What is a Scope? 
+the curly brackets ( {} ) are called scopes 
+
+If possible don't use var keyword 
+because of issue in bloack scope and functional scope 
+
+you can also declare a variable witthout using the keyword , but don't use this method.
+*/
