3a2eb539a90ecb9cd96c44493e85a6d4401de0c4
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-14 06:13:46 +0000

    container config done

---
+++ 01_Basics/01_Var.js
@@ -0,0 +1,13 @@
+const userId = 1001
+// const value can't be changed
+let email = "Shivi@abc.com"
+var password = "11224"
+userCity = "Shimla"
+
+//userId = 1 // can't be changed  
+email = "shiv@xyz.com"
+password = "2345"
+userCity = "Mohali"
+console.log(userId);
+console.table([email,password,userCity]);
+
