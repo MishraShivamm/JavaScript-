f1c15722dc9fa2ce0967a974decb59bd744251c1
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-15 06:03:27 +0000

    converssionOperations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -7,18 +7,36 @@ let valueInNumbers = Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn)
-
-
-
-
-
-
-
-
-
+//onsole.log(booleanIsLoggedIn)
 
 /* If the value of score is " String " and we have to work on numbers and  we are sure about it. So we'll declared " valueInNumber " using let 
-keyword. 
+keyword and same for other conversions. 
 Syntax example => let valueInNumbers = Number(score) 
-NaN => Not a number*/
+NaN => Not a numbe r*/
+
+
+
+// ************************************ OPERATIONS ****************************************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+// arithmatic operations
+//console.log(2 + 3);
+//console.log(2-3);
+//console.log(2*2);
+//console.log(2/2);
+
+let str1 = "hey"
+let str2 = " Mishre"
+
+let str3 = str1 + str2
+console.log(str3);
+
+//console.log(+true); // output = 1;
+//console.log(true+); // error
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
