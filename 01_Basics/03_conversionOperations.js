a3dff0c6b6578f1b5b1d436107e2e1555142e3ee
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-15 05:43:04 +0000

    conversionOperation

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,2 +1,24 @@
-let  score =  07
+let  score =  "07"
 
+//console.log(typeof score)
+let valueInNumbers = Number(score)
+//console.log(typeof valueInNumbers);
+//console.log(valueInNumbers);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+
+
+
+
+
+
+
+
+/* If the value of score is " String " and we have to work on numbers and  we are sure about it. So we'll declared " valueInNumber " using let 
+keyword. 
+Syntax example => let valueInNumbers = Number(score) 
+NaN => Not a number*/
