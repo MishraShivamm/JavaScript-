59ca1969e68ed7cb3f21edb619dd1d6fa7a81669
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-17 14:14:14 +0000

    array

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -1,5 +1,12 @@
 const heros = ["BAtman","Ironman"]
 const h2 = ["flash","Wonder Woman"]
 
-heros.push(h2)
-console.log(heros);
+//heros.push(h2)
+//console.log(heros);
+
+
+//const allheros = heros.concat(h2)
+//console.log(allheros);
+
+const h3 = [...heros,...h2]
+console.log(h3); 
