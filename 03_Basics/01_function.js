94a57dbd1093fdcd36735dec9a4fff5394fefc35
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-20 10:24:44 +0000

    function & Scope

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -26,4 +26,32 @@ function loginMessage(user= "Shivam"){
     }
     return '${"user"} logged in recently'
 }
-console.log(loginMessage());
+//console.log(loginMessage("Shivam"));
+
+function calculateCartPrice( val1, ...num1) {
+    return num1
+
+}
+//console.log(calculateCartPrice(2000,4940,3000));
+
+const user ={
+    name: "Shivam",
+    age: 21
+}
+
+function handleObject(anyObject){
+    console.log('Username is ${anyObject.name} and age  is {anyObject.age}');
+}
+
+handleObject({
+    name:"Shivam",
+    age: 21
+})
+
+
+const newArray = [1,2,4,5]
+
+function returnFromArray(getArray){
+    return getArray[1]
+}
+console.log(returnFromArray(newArray));
---
+++ 03_Basics/02_Scope.js
@@ -0,0 +1,54 @@
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "shivam"
+
+    function two(){
+        const Web = "Youtube"
+        console.log(username);
+    }
+    //console.log(Web);
+    two()
+}
+one()
+
+if(true){
+
+    const username1 = "Shivam"
+    if(username1 === "Shivam"){
+        const web1 = "Youtube"
+        console.log(username1+ web1);
+    }
+    //console.log(web1);
+}
+//console.log(username1);
+
+
+
+
+//++++++++++++++++++++++++++++++++++ INTERESTING +++++++++++++++++++++++++++++++++++++++++
+
+function add1(num1){
+    return num1 +1
+    
+
+}
+
+add1(5)
+
+
+const add2 = function (num1){
+    return num1 +2
+}
+add2(6)
+
+
+
