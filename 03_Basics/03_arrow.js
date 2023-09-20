01f4d6f9007dd9960eb7671bf72a5a4acb49e0d8
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-20 10:49:02 +0000

    arrow

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,15 @@
+const user ={
+    username:"Shivam",
+    price:"199"
+
+    wel: function (){
+        console.log('${this.username},welcome to Site');
+    }
+    
+}
+user.w()
+
+
+
+
+// this keyword reffers to the current context
