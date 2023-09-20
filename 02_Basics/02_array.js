c41e4aa27fc2d7f84bc5426273c9959cf73eedba
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-20 05:51:27 +0000

    array

--- 02_Basics/02_array.js
+++ 02_Basics/02_array.js
@@ -8,5 +8,24 @@ const h2 = ["flash","Wonder Woman"]
 //const allheros = heros.concat(h2)
 //console.log(allheros);
 
-const h3 = [...heros,...h2]
-console.log(h3); 
+const h3 = [...heros,...h2]// spread 
+//console.log(h3); 
+
+const h4 = [ 1,2,3,[4,5,6],7,[8,9,[3,4]]]
+
+const real_h4= h4.flat(Infinity)
+
+console.log(real_h4);
+
+console.log(Array.isArray("Shivam")); // output will be false because it's not an array
+console.log(Array.from("Shivam"));
+console.log(Array.from({name:"Shivam"})); // interesting
+
+let n1 = 10
+let n2 = 20
+let n3 = 30
+
+console.log(Array.of(n1,n2,n3));
+
+
+
