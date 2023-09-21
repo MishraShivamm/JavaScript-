d466480e14d7dcaafb652660a005fc54829255c4
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-21 09:29:32 +0000

    Controlflow

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,59 @@
+// If statement  
+/*
+const isUserloggedIn = true
+if (2 == "2"){
+    console.log("yes");
+
+} 
+else {
+    console.log("No");
+} */
+
+
+//const score = 200
+//if (score>100){
+//    const power = "Fly"
+//    console.log('user power: ${power}');
+//}
+    
+const UserloggedIn = true
+const debitcard = true
+
+if (UserloggedIn && debitcard){
+    console.
+}
+
+// Short hand notation 
+
+//const balance = 1000
+
+//if(balance >500) console.log("test"), console.log("implicit ");  Not good code 
+
+//Nesting 
+
+//if (balance < 500){
+//    console.log("Less than");
+//}  else if (balance <750){
+//    console.log("Less then 750");
+//}  else if (balance <900){
+//    console.log("less then 900");
+//} else {
+//    console.log("less then 1200");
+// }
+
+
+
+
+
+
+
+    
+    // if the condition is ture only then the code inside the if statement will run else it won't 
+
+/* comparison operator in if 
+< , > ,<= ,>= , == ,!=(not equal) , ===
+
+" = " it is used to assignment operator
+" == " it is used to check if the variables are equal or not 
+" === " it is used to check the variable's values as well as their data types. 
+*/
---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,36 @@
+/* Syntax 
+
+swithc(key){
+    ...... case value:
+
+    break;
+
+    default:
+      break;
+
+}
+*/
+
+
+const month = "March"
+switch (month) {
+    case "Jan":
+        console.log("January"); 
+        break;
+    case "Feb":
+        console.log("February"); 
+        break;
+    case "March":
+        console.log("March"); 
+        break;
+    case "APRIL":
+        console.log("April"); 
+        break;
+    case "June":
+        console.log("June"); 
+        break;
+
+    default:
+        console.log("non of them mathces");
+        break;
+}
