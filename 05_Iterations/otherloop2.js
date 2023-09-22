5bbbf22c88049bb886a826b5381e382228ea2ded
Shivam Mishra <91306181+MishraShivamm@users.noreply.github.com>
2023-09-22 05:32:51 +0000

    otherloop

--- 05_Iterations/otherloop2.js
+++ 05_Iterations/otherloop2.js
@@ -1,5 +1,44 @@
 const program =[" js" , "cpp","rb","swift"]
 
-program.forEach(  function (item) {
+//program.forEach(  function (item) {
+//    console.log(item);
+//})
+
+
+//program.forEach( (val) => {
+//    console.log(val);
+//}) 
+
+/*
+function printv(item) {
     console.log(item);
+    
+}
+
+program.forEach(printv)
+
+
+program.forEach((item ,  index , arr) => {
+    console.log(item , index , arr );
+})
+
+*/
+
+const code = [ {
+    language : " JavaScript",
+    file : "ES6"
+},
+{
+    language : " Java",
+    file : "JVM"
+},
+{
+    language : " cPP",
+    file : "c++"
+}
+]
+
+code.forEach( (item ,  index , arr) => {
+    console.log(item.language , index , arr);
+
 })
